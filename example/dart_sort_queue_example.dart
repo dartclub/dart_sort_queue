b9edf0bfcb98088e8e2f8525bda6233e01c0e278
Lukas Himsel <lukas@himsel.me>
2022-09-05 15:34:32 +0200

    prepare for release

--- example/dart_sort_queue_example.dart
+++ example/dart_sort_queue_example.dart
@@ -1,6 +1,33 @@
-// import 'package:dart_tiny_queue/dart_tiny_queue.dart';
+// ignore_for_file: unused_local_variable
 
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+import 'package:dart_sort_queue/dart_sort_queue.dart';
+
+main() {
+  // create a priority queue from an existing array (modifies the array)
+  dynamic queue = SortQueue<int>([7, 5, 10], (a, b) => a.compareTo(b));
+
+// add some items
+  queue.push(7);
+  queue.push(5);
+  queue.push(10);
+
+// remove the top item
+  var top = queue.pop(); // returns 5
+
+// return the top item (without removal)
+  top = queue.peek(); // returns 7
+
+// get queue length
+  queue.length; // returns 2
+
+// pass a custom item comparator as a second argument
+  queue = SortQueue<Map<String, int>>([
+    {"value": 5},
+    {"value": 7}
+  ], (a, b) {
+    return (a["value"] ?? 0) - (b["value"] ?? 0);
+  });
+
+// turn a queue into a sorted array
+  var list = queue.toList();
+}
